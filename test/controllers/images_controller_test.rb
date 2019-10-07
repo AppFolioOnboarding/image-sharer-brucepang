de16db0884631ea4b27a8695c7ed32ddab8843a5
Bo Pang <bo.pang@appfolio.com>
2019-10-07 15:28:03 -0700

    display image tags on index page

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -3,23 +3,28 @@ require 'test_helper'
 class ImagesControllerTest < ActionDispatch::IntegrationTest
   def test_index
     images = [
-      Image.create(url: 'https://google.com', created_at: 3.minutes.ago),
-      Image.create(url: 'https://facebook.com', created_at: 2.minutes.ago)
+      Image.create(url: 'https://google.com', created_at: 3.minutes.ago, tag_list: 'google'),
+      Image.create(url: 'https://facebook.com', created_at: 2.minutes.ago, tag_list: 'facebook, evil')
     ]
 
     get root_path
     assert_response :ok
 
-    assert_select '.image' do |elements|
+    assert_select '.js-image' do |elements|
       displayed_image_urls = elements.map { |element| element.attr('src') }
       assert_equal displayed_image_urls, images.reverse.map(&:url)
+    end
+
+    assert_select '.js-tag-list' do |elements|
+      displayed_image_tag_list = elements.map { |element| element.inner_text.strip }
+      assert_equal displayed_image_tag_list, (images.reverse.map { |image| image.tag_list.join(', ') })
     end
   end
 
   def test_index__wo_image
     get root_path
     assert_response :ok
-    assert_select '.image', 0
+    assert_select '.js-image', 0
   end
 
   def test_new
@@ -62,10 +67,10 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get image_path(image.id)
 
     assert_response :ok
-    assert_select '.image' do |element|
+    assert_select '.js-image' do |element|
       displayed_image_url = element.attr('src').value
       assert_equal displayed_image_url, image.url
     end
-    assert_select '.image_tag', 'google, search'
+    assert_select '.js-tag-list', 'google, search'
   end
 end
