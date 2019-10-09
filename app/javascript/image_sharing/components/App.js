e898a5a37865246aa88dace01abbcd0193bf6f1d
Bo Pang <bo.pang@appfolio.com>
2019-10-09 16:59:34 -0700

    add the form and footer

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import Header from './Header';
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
+      <Form>
+        <FormGroup>
+          <Label>
+            Your Name:
+            <Input type="text" name="name" />
+          </Label>
+        </FormGroup>
+        <FormGroup>
+          <Label>
+            Comments:
+            <Input type="textarea" name="comments" />
+          </Label>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+      <footer> Copyright: Appfolio Inc. Onboarding </footer>
     </div>
   );
 }
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -10,4 +10,15 @@ describe('<App />', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
   });
+
+  it('should have one input and one textarea', () => {
+    const wrapper = mount(<App />);
+    assert.strictEqual(wrapper.find('input').length, 1);
+    assert.strictEqual(wrapper.find('textarea').length, 1);
+  });
+
+  it('should have a footer', () => {
+    const wrapper = mount(<App />);
+    assert.strictEqual(wrapper.find('footer').length, 1);
+  });
 });
